e48e6c125fc63f3614359812e748d3e61c65ea68
Mihai Racoreanu <56828099+mi-rac@users.noreply.github.com>
2020-11-11 21:57:22 +0200

    Python-series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,7 @@
 def slices(series, length):
-    pass
+    if length > len(series):
+        raise ValueError('Length of subset cannot be larger than the one of the main set.')
+    elif length < 1:
+        raise ValueError('Length of subset cannot be 0 or negative')
+    else:
+        return [series[i:i+length] for i in range(len(series)-length+1)]
