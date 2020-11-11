9dcb62fc797efb032b152be6cf0f70fbcdad3a11
Mihai Racoreanu <56828099+mi-rac@users.noreply.github.com>
2020-11-11 22:17:43 +0200

    Python-matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = [[int(x) for x in row.split()] for row in matrix_string.splitlines()]
 
     def row(self, index):
-        pass
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        return [item[index-1] for item in self.matrix]
